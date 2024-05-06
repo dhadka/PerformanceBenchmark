b66aa898f2afc98671d5ba7e07720999a35b7a71
Dave Hadka <dhadka@github.com>
2024-05-05 20:12:50 -0600

    Add JMetal and JMetal-Plugin to benchmark

--- src/Benchmark.java
+++ src/Benchmark.java
@@ -1,18 +1,37 @@
-
-
+/* Copyright 2009-2024 David Hadka
+ *
+ * This file is part of the MOEA Framework.
+ *
+ * The MOEA Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * The MOEA Framework is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the MOEA Framework.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import java.io.IOException;
 
 import org.moeaframework.algorithm.NSGAII;
 import org.moeaframework.core.Algorithm;
 import org.moeaframework.core.Problem;
+import org.moeaframework.core.spi.AlgorithmFactory;
 import org.moeaframework.performance.DTLZ2WithCSocket;
 import org.moeaframework.performance.DTLZ2WithCStdio;
-import org.moeaframework.performance.DTLZ2WithJava;
 import org.moeaframework.performance.DTLZ2WithPypySocket;
 import org.moeaframework.performance.DTLZ2WithPypyStdio;
 import org.moeaframework.performance.DTLZ2WithPythonSocket;
 import org.moeaframework.performance.DTLZ2WithPythonStdio;
+import org.moeaframework.problem.DTLZ.DTLZ2;
 import org.moeaframework.util.Timing;
+
+import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAIIBuilder.NSGAIIVariant;
+import org.uma.jmetal.solution.doublesolution.DoubleSolution;
 
 public class Benchmark {
 	
@@ -28,6 +47,55 @@ public class Benchmark {
 		Timing.stopTimer(problem.getName());
 	}
 	
+	public static void runDirect() {
+		DTLZ2 problem = new DTLZ2(2);
+		
+		Timing.startTimer("Java");
+		
+		Algorithm algorithm = new NSGAII(problem);
+		algorithm.run(NFE);
+		
+		Timing.stopTimer("Java");
+	}
+	
+	// Construct and run JMetal directly
+	public static void runJMetalDirect() {
+		org.uma.jmetal.problem.multiobjective.dtlz.DTLZ2 problem =
+				new org.uma.jmetal.problem.multiobjective.dtlz.DTLZ2(11, 2);
+		
+		Timing.startTimer("JMetal (Direct)");
+		
+		org.uma.jmetal.operator.crossover.impl.SBXCrossover crossover = 
+				new org.uma.jmetal.operator.crossover.impl.SBXCrossover(1.0, 15.0);
+		
+		org.uma.jmetal.operator.mutation.impl.PolynomialMutation mutation =
+				new org.uma.jmetal.operator.mutation.impl.PolynomialMutation(1.0 / problem.numberOfVariables(), 20.0);
+		
+		org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAIIBuilder<DoubleSolution> builder =
+				new org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAIIBuilder<DoubleSolution>(
+						problem, crossover, mutation, 100);
+		
+		builder.setVariant(NSGAIIVariant.NSGAII);
+		builder.setMaxEvaluations(NFE);
+		
+        org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAII<DoubleSolution> algorithm = builder.build();
+        algorithm.run();
+		
+		Timing.stopTimer("JMetal (Direct)");
+	}
+	
+	// Construct and run JMetal using jmetal-plugin
+	public static void runJMetalPlugin() {
+		DTLZ2 problem = new DTLZ2(2);
+		
+		Timing.startTimer("JMetal (Plugin)");
+		
+		Algorithm algorithm = AlgorithmFactory.getInstance().getAlgorithm("NSGAII-JMetal", problem);
+		algorithm.run(NFE);
+		
+		Timing.stopTimer("JMetal (Plugin)");
+	}
+	
 	public static void main(String[] args) throws IOException {
 		if (args.length >= 1) {
 			N = Integer.parseInt(args[0]);
@@ -38,9 +106,7 @@ public class Benchmark {
 		}
 		
 		for (int i = 0; i < N; i++) {
-			try (Problem problem = new DTLZ2WithJava()) {
-				run(problem);
-			}
+			runDirect();
 			
 			try (Problem problem = new DTLZ2WithCStdio()) {
 				run(problem);
@@ -77,6 +143,9 @@ public class Benchmark {
 			try (Problem problem = new NativeFortran()) {
 				run(problem);
 			}
+			
+			runJMetalDirect();
+			runJMetalPlugin();
 		}
 		
 		Timing.display();
